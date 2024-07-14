efe5f6669113b2c0fb60e50b59b3ca03d2b53765
VineethaReddy18 <kavach1365@rgukt.ac.in>
2024-07-14 19:08:32 +0530

    updated

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.get('/twitter',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.send('<h1>please login</h1>')
 })
+app.get('/youtube',(req,res)=>{
+    res.send('youtube')
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
